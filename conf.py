6dad49d3bee29171058f5a302e2ecfff8b76f091
John Henderson <jw.hendy@gmail.com>
2017-05-15 18:30:05 -0500

    better title

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "John Henderson"  # (translatable)
-BLOG_TITLE = "technologeekery"  # (translatable)
+BLOG_TITLE = "jwhendy"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://jwhendy.github.io/"
